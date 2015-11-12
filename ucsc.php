69f678ffcaaf426265ab348e52e300d695ad104e
Lee <leewhite128@gmail.com>
2015-11-12 14:50:59 -0800

    text displays in header of admin page

--- ucsc.php
+++ ucsc.php
@@ -8,37 +8,28 @@ Author: Lee White
 Author URI: --
 */
 
-function hook_css() {
-
-	$output="<style> .wp_head_example { background-color : #f1f1f1; } </style>";
-
-	echo $output;
-
+function ucsc_header() {
+	echo "<p id='ucsc_header'>THIS IS SOME TEXT.</p>";
 }
 
-add_action('wp_head','hook_css');
+add_action( 'admin_notices', 'ucsc_header' );
 
-// function ucsc_header() {
-// 	echo "<p id='ucsc_header'>THIS IS SOME TEXT.</p>";
-// }
+function ucsc_footer() {
+	echo "<footer id='ucsc_footer'>THIS IS SOME OTHER TEXT.</footer>";
+}
 
-// add_action( 'wp_notices', 'ucsc_header' );
+add_action( 'admin_notices', 'ucsc_footer', 15 );
 
-// function ucsc_footer() {
-// 	echo "<footer id='ucsc_footer'>THIS IS SOME OTHER TEXT.</footer>";
-// }
+function footer_css() {
+	echo "
+	<style type='text/css'>
+	#ucsc_footer {
+		font-color: red;
+	}
+	</style>
+	";
+}
 
-// add_action( 'wp_notices', 'ucsc_footer', 15 );
+add_action( 'admin_head', 'footer_css' );
 
-// function footer_css() {
-// 	echo "
-// 	<style type='text/css'>
-// 	#ucsc_footer {
-// 		font-color: red;
-// 	}
-// 	</style>
-// 	";
-// }
-
-// add_action( 'wp_footer', 'footer_css' );
 ?>
